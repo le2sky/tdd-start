30f9e1c7a9641a3f98f78be557cda3fb43713391
leesky <leehaneul0623@gmail.com>
2023-02-17 01:21:12 +0900

    feat : 도난 카드번호를 위한 코드를 추가한 대역 구현

--- src/test/java/com/tdd/autodebit/AutoDebitRegister_Stub_Test.java
+++ src/test/java/com/tdd/autodebit/AutoDebitRegister_Stub_Test.java
@@ -1,6 +1,7 @@
 package com.tdd.autodebit;
 
 import static com.tdd.autodebit.CardValidity.INVALID;
+import static com.tdd.autodebit.CardValidity.THEFT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -27,5 +28,15 @@ class AutoDebitRegister_Stub_Test {
     RegisterResult result = register.register(req);
 
     assertEquals(INVALID, result.getValidity());
+  }
+
+  @Test
+  public void theftCard() throws Exception {
+    stubValidator.setTheftNo("1234567890123456");
+
+    AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+    RegisterResult result = register.register(req);
+
+    assertEquals(THEFT, result.getValidity());
   }
 }
