d7a61ffb3ec9691e37cc61a3e741088e3c0c9d94
leesky <leehaneul0623@gmail.com>
2023-02-17 00:52:49 +0900

    feat : jpa 레파지토리 구현

---
+++ src/main/java/com/tdd/autodebit/JpaAutoDebitInfoRepository.java
@@ -0,0 +1,15 @@
+package com.tdd.autodebit;
+
+public class JpaAutoDebitInfoRepository implements
+    AutoDebitInfoRepository {
+
+  @Override
+  public void save(AutoDebitInfo info) {
+
+  }
+
+  @Override
+  public AutoDebitInfo findOne(String userId) {
+    return null;
+  }
+}
