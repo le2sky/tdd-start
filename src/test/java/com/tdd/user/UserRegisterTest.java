6ce1440f7b8c44fa805033fc76ad4e2427820919
leesky <leehaneul0623@gmail.com>
2023-02-18 05:18:02 +0900

    test : fake 대역을 이용해서 아이디 중복 검증 테스트 코드 작성

--- src/test/java/com/tdd/user/UserRegisterTest.java
+++ src/test/java/com/tdd/user/UserRegisterTest.java
@@ -11,10 +11,11 @@ public class UserRegisterTest {
 
   private UserRegister userRegister;
   private StubWeakPasswordChecker stubWeakPasswordChecker = new StubWeakPasswordChecker();
+  private MemoryUserRepository fakeRepository = new MemoryUserRepository();
 
   @BeforeEach
   void setUp() {
-    userRegister = new UserRegister(stubWeakPasswordChecker);
+    userRegister = new UserRegister(stubWeakPasswordChecker, fakeRepository);
   }
 
   @DisplayName("약한 암호면 가입 실패")
@@ -24,6 +25,16 @@ public class UserRegisterTest {
 
     assertThrows(WeakPasswordException.class, () -> {
       userRegister.register("id", "pw", "email");
+    });
+  }
+
+  @DisplayName("이미 같은 ID가 존재하면 가입 실패")
+  @Test
+  public void dupIdExists() throws Exception {
+    fakeRepository.save(new User("id", "pw1", "email@email.com"));
+
+    assertThrows(DupIdException.class, () -> {
+      userRegister.register("id", "pw2", "email");
     });
   }
 }
